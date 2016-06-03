c2c019fc9c1f36386589688131ba04b541d8c39c
Wangxin <jiugewx@qq.com>
2016-06-03 16:43:29 +0800

    'one'

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
-/**
- * Created by Xinye on 2016/5/18.
- */
-var webpack = require('webpack');
-var path = require('path');
 
-var ROOT_PATH = path.resolve(__dirname);
-var DIST_PATH = path.resolve(ROOT_PATH, 'dist');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");/*单独打包工具*/
+var path = require('path');
 
 module.exports = {
     devtool: false,/*生产环境，使用devtool:false这样打包出来的js会很小，开发环境，使用devtool: 'source-map'*/
@@ -18,25 +14,26 @@ module.exports = {
     output: {
         path:path.join(__dirname, 'dist/'),
         filename: 'main.js',
-        publicPath: '/dist/'
+        publicPath: ''/*发布路径一定要对，否则到时候在服务器上无法引用png,js等*/
     },
     module: {
         loaders: [
-            {test: /\.css$/, loaders: ['style', 'css'],exclude: /node_modules/},
-            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+            {test: /\.(woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'url-loader?importLoaders=1&limit=1000&name=/fonts/[name].[ext]'},/*让webpack帮助复制font文件*/
+            {test:/\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader"),exclude: /node_modules/},
+            {test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192&name=./images/[name].[ext]?[hash:10]'}
         ]
     },
     resolve: {
         /*自动添加上后缀*/
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx',"css","png","jpg","jpeg"]
     },
     plugins: [
         /*加上热替换的插件和防止报错的插件*/
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin("main.css")
     ],
     devServer: {
-        //contentBase: './dist',/*所有的文件都在这个下面包括html、jpg、js等*/
         historyApiFallback: true,
         devtool: eval,
         progress: true,
@@ -45,6 +42,7 @@ module.exports = {
         profile: true
     },
     externals: [{
+        //"$":"jQuery"
         /* 'react-dom': 'ReactDOM',
          'react': 'React'*/
     }]
