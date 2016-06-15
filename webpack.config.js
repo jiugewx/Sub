ecd0e1beff3fbd8f3d3a7e5beb9fccc6f3f2e1a6
xinye <jiugewx@qq.com>
2016-06-15 20:00:33 +0800

    new webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,100 @@
 var webpack = require('webpack');
-var distConfigs=require("./webpack.dist.config.js");
+var distConfigs = require("./webpack.dist.config.js");
 /**
  html-webpack-plugin插件，重中之重，webpack中生成HTML的插件，
  具体可以去这里查看https://www.npmjs.com/package/html-webpack-plugin
  */
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+/*设置环境变量--开发环境(这个定义位置只能放在这里)*/
+process.env.NODE_ENV = 'development';
+var development= definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.NODE_ENV === 'development' || 'true'))
+});
 
-module.exports= {
-    entry:distConfigs.entry,
-    plugins: [
-        //HtmlWebpackPlugin，模板生成相关的配置，每个对于一个页面的配置，有几个写几个
-        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
-            favicon: './src/img/subway/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
-            template: './index.html', //html来源
-            filename: './index.html', //生成的html存放路径，相对于path
-            chunks: ['common', 'index'],//需要引入的chunk，不配置就会引入所有页面的资源
-            inject: 'body', //js插入的位置，true/'head'/'body'/false
-            hash: true, //为静态资源生成hash值
-            minify: { //压缩HTML文件
-                removeComments: true, //移除HTML中的注释
-                collapseWhitespace: true //删除空白符与换行符
-            }
-        }),
-
+var defaultPre = {
+    all_in_one: distConfigs.userOptions.all_in_one,
+    chunks: distConfigs.userOptions.chunks,
+    Plugins: [
+        development,
         /*加上热替换的插件和防止报错的插件以下都会写进js */
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
-
-    ].concat(distConfigs.plugins),
-    devtool:false,
-    output:distConfigs.output,
-    module:distConfigs.module,
-    resolve:distConfigs.resolve,
-    devServer:distConfigs.devServer,
-    externals:distConfigs.externals
+    ].concat(distConfigs.userOptions.plugins),
+    entry: distConfigs.entry,
+    init: function () {
+        var self = this;
+        console.log("default-NODE_ENV", process.env.NODE_ENV);
+        //self.setEntry();
+        for (var i in self.chunks) {
+            if (!self.all_in_one) {
+                var htmlPlugins = new HtmlWebpackPlugin({
+                    //favicon: './src/img/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
+                    template: './src/' + self.chunks[i] + ".html", //html来源
+                    filename: './' + self.chunks[i] + '.html', //生成的html存放路径，相对于path
+                    chunks: [self.chunks[i], 'common'],//需要引入的chunk，不配置就会引入所有页面的资源
+                    inject: 'body', //js插入的位置，true/'head'/'body'/false
+                    hash: true, //为静态资源生成hash值
+                    minify: { //压缩HTML文件
+                        removeComments: false, //移除HTML中的注释
+                        collapseWhitespace: false //删除空白符与换行符
+                    }
+                });
+                self.Plugins.push(htmlPlugins);
+            } else if (self.all_in_one) {
+                var htmlPlugins = new HtmlWebpackPlugin({
+                    //favicon: './src/img/favicon.ico',
+                    template: './src/' + self.chunks[i] + ".html",
+                    filename: './' + self.chunks[i] + '.html',
+                    inject: 'body',
+                    hash: true,
+                    minify: {
+                        removeComments: true,
+                        collapseWhitespace: true
+                    }
+                });
+                self.Plugins.push(htmlPlugins);
+            }
+        }
+    }
 };
+defaultPre.init();
+var defaultConfig = {};
+if (process.env.NODE_ENV !== 'production') {
+    console.log("the NODE_ENV is development");
+    defaultConfig = {
+        entry: defaultPre.entry,
+        plugins: defaultPre.Plugins,
+        devtool: "cheap-source-map",
+        output: distConfigs.output,
+        module: distConfigs.module,
+        resolve: distConfigs.resolve,
+        //使用webpack-dev-server，提高开发效率
+        devServer: {
+            historyApiFallback: true,
+            contentBase: './dist/', /*所有的文件都要放在dist下面*/
+            host: 'localhost',
+            port: 8080,
+            inline: true,
+            devtool: eval,
+            progress: true,
+            colors: true,
+            hot: true,
+            profile: true
+        },
+        externals: distConfigs.externals
+    }
+}
+if (process.env.NODE_ENV === 'production') {
+    console.log("the NODE_ENV is production");
+    defaultConfig = {
+        entry: distConfigs.entry,
+        plugins: distConfigs.plugins,
+        devtool: distConfigs.devtool,
+        output: distConfigs.output,
+        module: distConfigs.module,
+        resolve: distConfigs.resolve,
+        externals: distConfigs.externals
+    };
+}
+
+module.exports=defaultConfig;
--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -6,30 +6,114 @@ var webpack = require('webpack');
  妈妈再也不用担心样式会被打包到js文件里了。
  */
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+/*设置环境变量--生产环境(这个定义位置只能放在这里)*/
+process.env.NODE_ENV = 'production';
+var production = new webpack.DefinePlugin({
+    __DEV__:JSON.stringify(JSON.parse(process.env.NODE_ENV === 'development' || 'false')),
+    "process.env":{
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')}
+});
 
-
-var distConfigs = {
-    devtool:false,
-    entry: { //配置入口文件，有几个写几个
+var preConfig={
+    all_in_one: true,/*是否只输出一个js/css,还是要输出一个html对应多个js/css*/
+    one_js: {entry: "./src/index.js"},
+    all_js: {
+        /*配置入口文件，有几个写几个*/
         index: './src/index.js'
     },
+    entry:{},
+    output:{},
+    commonChunks:{},
+    commonPlugins:[],
+    ExtractPlugin:[new ExtractTextPlugin('./css/main.css')],
+    plugins: [],
+    init:function(){
+        var self=this;
+        console.log("dist-NODE_ENV", process.env.NODE_ENV);
+        self.getEntry();
+        self.getOutput();
+        self.getoutputChunks();
+        self.getCommonPlugins();
+        self.getPlugins();
+    },
+    getEntry:function(){
+        var self=this;
+        if(self.all_in_one){
+            self.entry=self.one_js;
+        }else{
+            self.entry=self.all_js;
+        }
+    },
+    getOutput:function(){
+        var self=this;
+        if(self.all_in_one){
+            self.output={
+                path: path.join(__dirname, 'dist/'), /*输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它*/
+                publicPath: '', /*模板、样式、脚本、图片等资源对应的server上的路径,所有的资源引用添加头*/
+                filename: 'js/main.js', /*每个页面对应的主js的生成配置*/
+                chunkFilename: 'js/[chunkhash:8].js'   /*chunk生成的配置*/
+            };
+        }else{
+            self.output= {
+                path: path.join(__dirname, 'dist/'),
+                publicPath: '',
+                filename: 'js/[name].js',
+                chunkFilename: 'js/[chunkhash:8].js'
+            };
+        }
+    },
+    getoutputChunks:function () {
+        var self=this;
+        var _chunks = [];
+        for (var key in self.all_js) {
+            _chunks.push(key);
+        }
+        self.commonChunks.chunks = _chunks;
+        self.commonChunks.minChunks = _chunks.length;
+    },
+    getCommonPlugins: function () {
+        var self=this;
+        var _chunks=self.commonChunks.chunks,
+            _minChunks=self.commonChunks.minChunks;
+        self.commonPlugins=[new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            chunks: _chunks,
+            minChunks: _minChunks
+        }), new ExtractTextPlugin('./css/[name].css')];
+    },
+    getPlugins: function () {
+        var self=this;
+        if(self.all_in_one){
+            self.plugins=self.plugins.concat(self.ExtractPlugin);
+        }else{
+            self.plugins=self.plugins.concat(self.commonPlugins);
+        }
+    }
+};
+preConfig.init();
+
+var distConfigs = {
+    devtool: false,
+    entry: preConfig.entry,
+    output: preConfig.output,
+    plugins: [
+        production,
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})/*压缩时不要打印警告*/
+    ].concat(preConfig.plugins),
     module: {
         loaders: [ //加载器，关于各个加载器的参数配置，可自行搜索之。
             {
                 test: /\.css$/,
                 //配置css的抽取器、加载器。'-loader'可以省去
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                loader: ExtractTextPlugin.extract('style', 'css', {publicPath: '../'}/*给css中的src引用添加地址头*/)
             }, {
                 test: /\.less$/,
                 //配置less的抽取器、加载器。中间!有必要解释一下，
                 //根据从右到左的顺序依次调用less、css加载器，前一个的输出是后一个的输入
                 //你也可以开发自己的loader哟。有关loader的写法可自行谷歌之。
-                loader: ExtractTextPlugin.extract('css!less')
-            }, {
-                //html模板加载器，可以处理引用的静态资源，默认配置参数attrs=img:src，处理图片的src引用的资源
-                //比如你配置，attrs=img:src img:data-src就可以一并处理data-src引用的资源了，就像下面这样
-                test: /\.html$/,
-                loader: "html?attrs=img:src img:data-src"/*这样会压缩html中的图片*/
+                loader: ExtractTextPlugin.extract('css!less', {publicPath: '../'})
             }, {
                 //文件加载器，处理文件静态资源
                 test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -38,46 +122,32 @@ var distConfigs = {
                 //图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
                 //如下配置，将小于8192byte的图片转成base64码，
                 test: /\.(png|jpg|gif)$/,
-                loader: 'url-loader?limit=20480&name=./images/[name].[ext]?[hash:8]'
-                /*这里是默认的图片引用地址，并且也是相对于publish的存放地址。
+                loader: 'url-loader?limit=10240&name=images/[name].[ext]?[hash:8]'
+                /*这里是css中图片的引用地址，并且也是相对于publish的存放地址。
                  在css中的效果就是  publish+url（因此要注意css的引用地址，及本身位置）*/
+            },
+            {
+                //html模板加载器，可以处理引用的静态资源，默认配置参数attrs=img:src，处理图片的src引用的资源
+                //比如你配置，attrs=img:src img:data-src就可以一并处理data-src引用的资源了，就像下面这样
+                test: /\.html$/,
+                loader: "html?attrs=img:src img:data-src"/*这样会压缩html中的图片*/
             }
         ]
     },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common', // 将公共模块提取，生成名为`vendors`的chunk
-            chunks: ['index', 'list', 'about'], //提取哪些模块共有的部分
-            minChunks: 3 // 提取至少3个模块共有的部分
-        }),
-        new ExtractTextPlugin('./[name].css') //单独使用link标签加载css并设置路径，相对于output配置中的publickPath
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
-        publicPath: '',				//模板、样式、脚本、图片等资源对应的server上的路径,
-        // 其中css中的背景图片的路径引用是相对于这个的。所有的url都会以这个为前缀
-        filename: 'js/[name].js',			//每个页面对应的主js的生成配置
-        chunkFilename: 'js/[id].chunk.js'   //chunk生成的配置
-    },
-    //使用webpack-dev-server，提高开发效率
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './dist/',/*所有的文件都要放在dist下面*/
-        host: 'localhost',
-        port: 8080,
-        inline: true,
-        devtool: eval,
-        progress: true,
-        colors: true,
-        hot: true,
-        profile: true
-    },
     externals: [{
         /*不打包的库，html中使用CDN*/
-        //jquery:"$"
-        /* 'react-dom': 'ReactDOM',
-         'react': 'React'*/
-    }]
+        // 'jquery':"$"
+        // 'react-dom': 'ReactDOM',
+        // 'react': 'React'
+    }],
+    resolve: {
+        extensions: ['', '.jsx', '.js', '.css', '.png', '.jpg'],
+        modulesDirectories: ['shared', 'node_modules']
+    },
+    userOptions: {
+        all_in_one: preConfig.all_in_one,
+        chunks: preConfig.commonChunks.chunks,
+        plugins: preConfig.plugins
+    }
 };
-
-module.exports=distConfigs;
+module.exports = distConfigs;
